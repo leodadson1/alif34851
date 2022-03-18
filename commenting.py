6be933e92f4704b210480cf8b9833f1a26d5e881
leodadson1 <101863737+leodadson1@users.noreply.github.com>
2022-03-18 09:14:38 +0000

    Update commenting.py

--- commenting.py
+++ commenting.py
@@ -7,23 +7,39 @@ class MyStreamListener(tweepy.StreamListener):
         self.me = api.me()
 
     def on_status(self, tweet):
+        print("Gotten some")
         tweet_id=tweet.id
-        print(tweet_id)
-        # api.retweet(tweet_id)
+        #########COMMENT######
+        comment="""
+            I am just testing if My bot would work or not?
+        """
+
+        if tweet.favorite_count >=0 or  tweet.retweet_count >=0:
+            print(tweet.favorite_count)
+            print("The tweet is greater than 0 or equal to 0  ")
+        api.update_status(status=comment, in_reply_to_status_id = tweet_id, auto_populate_reply_metadata=True)
+
         
-
-
     def on_error(self, status):
         print("Error detected")
 
+API_KEY="WI1tfVUIg0IJaQ1nuxS0WqnSX"
+API_SECRET_KEY="0OusFSDVi4qy9akDZuBYSfxuTeQ1TSEdgm1tJUisFAfD4Zdmag"
+ACCESS_TOKEN=""
+ACCESS_TOKEN_SECRET=""
+
+
 # Authenticate to Twitter
-auth = tweepy.OAuthHandler("2eVT67VNmTdY3rpzt2BHdWVhL", "B2ttH8vl78eIyS2DmLXMNxB4vAh7DhCEajn5GICSkB4fKdrPyS")
-auth.set_access_token("1075083604353564672-xTHmNCFI5SHFUJu1HwLgp5ERCJ8ecW", "FkYRE8N06DRyNYCTpq3CBWcL5rG9BMFGBaA7Jd2aBudYQ")
+auth = tweepy.OAuthHandler(API_KEY, API_SECRET_KEY)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 # Create API object
 api = tweepy.API(auth, wait_on_rate_limit=True,
     wait_on_rate_limit_notify=True)
 
+
+
 tweets_listener = MyStreamListener(api)
 stream = tweepy.Stream(api.auth, tweets_listener)
-stream.filter(track=["Python", "Django", "Tweepy"], languages=["en"])
+######Hashtags#####
+stream.filter(track=["Python", "#btc", "bitcoin"], languages=["en"])

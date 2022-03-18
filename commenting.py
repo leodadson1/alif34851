f5d08f78f70c19d9b5b2c36e8d3a2bab7cba27e5
leodadson1 <leodadsonnnn@gmail.com>
2022-03-18 08:36:26 +0000

    first deploy

---
+++ commenting.py
@@ -0,0 +1,44 @@
+import json
+import tweepy
+
+class MyStreamListener(tweepy.StreamListener):
+    def __init__(self, api):
+        self.api = api
+        self.me = api.me()
+
+    def on_status(self, tweet):
+        print("Gotten some")
+        tweet_id=tweet.id
+        #########COMMENT######
+        comment="""
+            I am just testing if My bot would work or not?
+        """
+
+        if tweet.favorite_count >=0 or  tweet.retweet_count >=0:
+            print(tweet.favorite_count)
+            print("The tweet is greater than 0 or equal to 0  ")
+        # api.update_status(status=comment, in_reply_to_status_id = tweet_id, auto_populate_reply_metadata=True)
+
+        
+    def on_error(self, status):
+        print("Error detected")
+
+API_KEY="WI1tfVUIg0IJaQ1nuxS0WqnSX"
+API_SECRET_KEY="0OusFSDVi4qy9akDZuBYSfxuTeQ1TSEdgm1tJUisFAfD4Zdmag"
+ACCESS_TOKEN=""
+ACCESS_TOKEN_SECRET=""
+
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler(API_KEY, API_SECRET_KEY)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+# Create API object
+api = tweepy.API(auth, wait_on_rate_limit=True,
+    wait_on_rate_limit_notify=True)
+
+
+
+tweets_listener = MyStreamListener(api)
+stream = tweepy.Stream(api.auth, tweets_listener)
+stream.filter(track=["Python", "#btc", "bitcoin"], languages=["en"])
